d6ee27cc6f5e3a11520aba8d82fa43ca9afdbbf3
DylanWhite2246Programming <whited200550@gmail.com>
2022-01-25 22:09:02 -0500

    made into a subsystem

---
+++ src/main/java/frc/robot/team2246/NetworktableHandeler.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.team2246;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class NetworktableHandeler extends SubsystemBase {
+  /** Creates a new NetworkTableHandeler. */
+  public NetworktableHandeler() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
